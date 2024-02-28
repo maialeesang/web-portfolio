682281e2ce99e23d712934298bef7b2141ee53d1
maialeesang <leesang.maia@gmail.com>
2024-02-28 16:36:15 -0500

    edits and changes
    added function components, updated short blurb, added a nav, changed background photo

--- src/App.js
+++ src/App.js
@@ -3,23 +3,59 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            "url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)",
-        }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Hi, my name is Maia and this is my web design portfolio.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
-          </div>
+    <div className="container">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="navbar bg-neutral text-neutral-content">
+      <div className="flex-1">
+        <a className="btn btn-ghost text-xl">Maia Lee Sang</a>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+function Hero() {
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1460039230329-eb070fc6c77c?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+      }}
+    >
+      <div className="hero-overlay bg-opacity-40"></div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-5xl font-bold">Hello!</h1>
+          <p className="mb-5 font-bold">
+            Hi, my name is Maia and welcome to my web design portfolio. Here, I
+            showcase my past projects and career background that has shaped my
+            web development experience.
+          </p>
+          <button className="btn btn-primary">Explore</button>
         </div>
       </div>
     </div>
